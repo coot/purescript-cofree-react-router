33927f478724d5ecf046f5520712b3860eeaa364
Marcin Szamotulski <mszamot@gmail.com>
2017-03-06 20:11:35 +0000

    Add test to find a route in a different branch.

--- test/Router.purs
+++ test/Router.purs
@@ -22,29 +22,43 @@ foreign import unsafeGetId :: ReactElement -> String
 testSuite :: forall eff. TestSuite eff
 testSuite =
     suite "Router" do
-        suite "runRouter" do
-            test "should find patterns"
-                let router :: Router
-                    router = (Route "main" "/" routeClass) :<
-                                [ (Route "home" "home" routeClass) :< []
-                                , (Route "user" "user/:user_id" routeClass) :<
-                                    [ (Route "books" "books" routeClass) :< []
-                                    , (Route "book" "books/:book_id" routeClass) :< []
-                                    ]
-                                , (Route "user-settings" "user/:user_id/settings" routeClass) :< []
+        suite "runRouter"
+            let router :: Router
+                router = (Route "main" "/" routeClass) :<
+                            [ (Route "home" "home" routeClass) :< []
+                            , (Route "user" "user/:user_id" routeClass) :<
+                                [ (Route "books" "books" routeClass) :< []
+                                , (Route "book" "books/:book_id" routeClass) :< []
                                 ]
-                    check url expected = 
-                        let res = runRouter url router
-                         in case res of
-                            Nothing -> failure $ "router did not find <" <> url <> ">"
-                            Just (Triple cls props children) -> 
-                                let el = createElement cls props children
-                                    ident = unsafeGetId el
-                                in assert ("expected id '" <> expected <> "' got '" <> ident <> "'") (ident == expected)
-                 in do
-                    check "/" "main"
-                    check "/home" "home"
-                    check "/user/2" "user"
-                    check "/user/2/books" "books"
-                    check "/user/2/books/3" "book"
-                    check "/user/2/settings" "user-settings"
+                            , (Route "user-settings" "user/:user_id/settings" routeClass) :< []
+                            ]
+
+                router2 :: Router
+                router2 = (Route "main" "/" routeClass) :<
+                            [ (Route "home" "home" routeClass) :< 
+                                [ (Route "user" "user" routeClass) :< []
+                                ]
+                            , (Route "user-settings" "home/user/settings" routeClass) :< []
+                            ]
+
+                check router url expected = 
+                    let res = runRouter url router
+                     in case res of
+                        Nothing -> failure $ "router did not find <" <> url <> ">"
+                        Just (Triple cls props children) -> 
+                            let el = createElement cls props children
+                                ident = unsafeGetId el
+                            in assert ("expected id '" <> expected <> "' got '" <> ident <> "'") (ident == expected)
+            in do
+                test "should find patterns" do
+                    check router "/" "main"
+                    check router "/home" "home"
+                    check router "/user/2" "user"
+                    check router "/user/2/books" "books"
+
+                test "should find a route if a less speicalized one hides it" do
+                    check router "/user/2/settings" "user-settings"
+                    check router "/user/2/books/3" "book"
+
+                test "find a route in a different branch" do
+                    check router2 "/home/user/settings" "user-settings"
