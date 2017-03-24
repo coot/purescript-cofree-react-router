6832f63b2a39ef0ed2aab3d37629d1585e3f1710
Marcin Szamotulski <mszamot@gmail.com>
2017-03-24 13:20:18 +0000

    Routeing.shake - not fully matched urls

--- src/React/Router/Routing.purs
+++ src/React/Router/Routing.purs
@@ -5,9 +5,10 @@ module React.Router.Routing
 
 import Prelude
 import Data.Array as A
+import Data.List as L
 import React.Router.Utils as U
 import Control.Comonad.Cofree (Cofree, head, tail, (:<))
-import Data.Either (Either(..))
+import Data.Either (Either(..), either)
 import Data.Foldable (foldl)
 import Data.Maybe (Maybe(..), maybe)
 import Data.Newtype (wrap)
@@ -18,7 +19,15 @@ import Optic.Setter (set)
 import React (ReactElement, createElement)
 import React.Router.Match (runMatch)
 import React.Router.Types (IndexRoute(..), Route(..), RouteProps_(..), RouteProps, Router, URL, idLens, urlLens)
+import Routing.Match.Class (lit)
 import Routing.Parser (parse) as R
+import Routing.Types (RoutePart(..))
+
+routeToString :: L.List RoutePart -> String
+routeToString url = L.intercalate "/" $ unwrap <$> url
+  where
+    unwrap (Path p) = p
+    unwrap (Query q) = "?" <> (show q)
 
 -- | remove all branches that are annotated with `Nothing`
 -- | it should also elminate not fully consumed URLs
@@ -28,8 +37,17 @@ shake
   -> Maybe (Cofree Array {url :: URL, props :: (RouteProps_ args) | a})
 shake cof = case head cof of
     Nothing -> Nothing
-    Just a -> Just (a :< go (tail cof))
+    Just r ->
+      let tail_ = go (tail cof)
+      in if A.length tail_ /= 0 || matchEnd r.url
+           then Just (r :< tail_)
+           else Nothing
   where
+    matchEnd :: URL -> Boolean
+    matchEnd url
+      = L.length url == 0
+        || L.length url == 1
+        && (either (const false) (const true) $ runMatch (unit <$ lit "") url)
     go
       :: Array (Cofree Array (Maybe {url :: URL, props :: (RouteProps_ args) | a }))
       -> Array (Cofree Array {url :: URL, props :: (RouteProps_ args) | a})
@@ -37,7 +55,11 @@ shake cof = case head cof of
       where 
         f cofs cof = case head cof of
                        Nothing -> cofs
-                       Just cofHead -> A.snoc cofs (cofHead :< go (tail cof))
+                       Just cofHead ->
+                         let tail_ = go $ tail cof
+                         in if A.length tail_ /= 0 || matchEnd cofHead.url
+                              then A.snoc cofs (cofHead :< go (tail cof))
+                              else cofs
 
 matchRouter
   :: forall args
@@ -53,12 +75,10 @@ matchRouter url router = case shake $ go url router of
     check :: Route args
           -> URL
           -> Maybe {url :: URL, props :: (RouteProps_ args)}
-    check route url =
+    check route@(Route idRoute _ _) url =
       case runMatch (view urlLens route) url of
            Left _ -> Nothing
-           Right (Tuple urlRest args) ->
-             case route of
-                  Route id _ _ -> Just { url: urlRest , props: wrap { id, args } }
+           Right (Tuple urlRest args) -> Just { url: urlRest , props: wrap { id: idRoute, args } }
 
     -- traverse Cofree and match routes
     go
