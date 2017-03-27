937f5b9bfda9be344ddfd82744816edc2dbc4563
Marcin Szamotulski <mszamot@gmail.com>
2017-03-27 17:20:02 +0000

    export React.Router.Class from React.Router

--- src/React/Router.purs
+++ src/React/Router.purs
@@ -2,8 +2,10 @@ module React.Router
   ( module React.Router.Components
   , module React.Router.Routing
   , module React.Router.Types
+  , module React.Router.Class
   ) where
 
 import React.Router.Components
 import React.Router.Routing
 import React.Router.Types
+import React.Router.Class
