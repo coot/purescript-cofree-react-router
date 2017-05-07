2b0bcafadf3215ecde489c548a6feea5e3c7f312
Marcin Szamotulski <mszamot@gmail.com>
2017-05-07 11:58:51 +0200

    remove unsused functions from React.Router.Utils

--- src/React/Router/Utils.purs
+++ src/React/Router/Utils.purs
@@ -7,25 +7,6 @@ import Data.Maybe (Maybe(..))
 import Data.NonEmpty (NonEmpty, (:|))
 import Routing.Types (Route, RoutePart(..)) as R
 
--- | get last element on `NonEmpty Array a`
--- | this is useful to get the route args for the current args inside
--- | `RouteClass` component:
--- | ```purescript
--- |    do
--- |      props <- getProps this
--- |      let arg = last props.args
--- | ```
-last :: forall a. NonEmpty Array a -> a
-last (a :| as) =
-  case A.last as of
-    Nothing -> a
-    Just l -> l
-
-snoc :: forall a. Array a -> a -> NonEmpty Array a
-snoc as a = case A.uncons as of
-              Nothing -> a :| []
-              Just {head, tail} -> head :| (A.snoc tail a)
-
 -- | Print `Routing.Types.Route` as a string,  useful for debugging.
 routeToString :: R.Route -> String
 routeToString url = L.intercalate "/" $ unwrap <$> url
