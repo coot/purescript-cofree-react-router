05f0b3b1bf5a03ef6189d29356de6c5564905e19
Marcin Szamotulski <mszamot@gmail.com>
2017-06-28 13:35:56 +0200

    showLocation

--- src/React/Router/Utils.purs
+++ src/React/Router/Utils.purs
@@ -1,11 +1,15 @@
 module React.Router.Utils where
 
 import Prelude
-import Data.List as L
-import Data.String as S
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
+import Data.Foldable (class Foldable, foldMap)
+import Data.List as L
 import Data.Maybe (Maybe(..), isJust)
+import Data.Monoid (class Monoid)
+import Data.Newtype (class Newtype, ala)
+import Data.String as S
 import Routing.Types (Route, RoutePart(..)) as R
 
 -- | Print `Routing.Types.Route` as a string,  useful for debugging.
@@ -33,12 +37,26 @@ joinUrls a b =
   if S.null a
     then b
     else
-      let _a = if S.charAt (S.length a - 1) a == Just '/'
-                 then S.take (S.length a - 1) a
-                 else a
-          _b = if S.charAt 0 b == Just '/'
-                 then S.drop 1 b
-                 else b
-      in _a <> "/" <> _b
+      if S.null b
+        then a
+        else
+          let _a = if S.charAt (S.length a - 1) a == Just '/'
+                    then S.take (S.length a - 1) a
+                    else a
+              _b = if S.charAt 0 b == Just '/'
+                    then S.drop 1 b
+                    else b
+          in _a <> "/" <> _b
 
-  
+newtype URLPart = URLPart String
+
+derive instance newtypeURLpart :: Newtype URLPart _
+
+instance semigroupURLPart :: Semigroup URLPart where
+  append (URLPart a) (URLPart b) = URLPart $ joinUrls a b
+
+instance monoidURLPart :: Monoid URLPart where
+  mempty = URLPart ""
+
+showLocation :: forall a t. Show a => Foldable t => Functor t => t a -> String
+showLocation t = ala URLPart foldMap (show <$> t)
--- test/Utils.purs
+++ test/Utils.purs
@@ -2,10 +2,20 @@ module Test.Utils
   ( testSuite ) where
 
 import Prelude
+
+import Data.List (List(..), (:))
 import Data.Maybe (Maybe(Just, Nothing))
-import React.Router.Utils (hasBaseName, joinUrls, stripBaseName)
+import Data.Monoid (mempty)
+import React.Router.Utils (URLPart(..), hasBaseName, joinUrls, showLocation, stripBaseName)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert (assert)
+
+data Location = Home | User Int | Settings
+
+instance showLocation :: Show Location where
+  show Home = "/home"
+  show (User id) = "user/" <> show id
+  show (Settings) = "settings/"
 
 testSuite :: forall eff. TestSuite eff
 testSuite = suite "Utils" do
@@ -29,6 +39,27 @@ testSuite = suite "Utils" do
     let r2 = joinUrls "/a/" "/b"
     assert ("expected /a/b but got " <> r2) $ r2 == "/a/b"
     let r3 = joinUrls "/a/" "b" 
-    assert ("expected /a/b bot got " <> r3) $ r3 == "/a/b"
+    assert ("expected /a/b but got " <> r3) $ r3 == "/a/b"
     let r4 = joinUrls "" "/b"
-    assert ("expected /b bot got " <> r4) $ r4 == "/b"
+    assert ("expected /b but got " <> r4) $ r4 == "/b"
+    let r5 = joinUrls "/a" ""
+    assert ("expected /a but got " <> r5) $ r5 == "/a"
+
+  test "URLParts" do
+    let URLPart r1 = URLPart "/" <> mempty
+        e1 = "/"
+    assert "" $ r1 == e1
+    let URLPart r2 = mempty <> URLPart "/"
+        e2 = "/"
+    assert "" $ r2 == e2
+
+  test "showLocation" do
+    let r1 = showLocation (Home : User 1 : Nil)
+        e1 = "/home/user/1"
+    assert ("expected " <> e1 <> " but got " <> r1) $ r1 == e1
+    let r2 = showLocation (Home : User 1 : Settings : Nil)
+        e2 = "/home/user/1/settings/"
+    assert ("expected " <> e2 <> " but got " <> r2) $ r2 == e2
+    let r3 = showLocation [Home, Settings, User 1]
+        e3 = "/home/settings/user/1"
+    assert ("expected " <> e3 <> " but got " <> r3) $ r3 == e3
